53967056540864c9e2b183aa0df5b28a06c416a3
SiglabsOP <spectralpulse.456789012@gmail.com>
2024-12-19 15:44:42 +0100

    monitor integration

--- NebulaPro.py
+++ NebulaPro.py
@@ -2,6 +2,6 @@
 import marshal
 import os
 
-s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '__custom_pycache__', 'NebulaPro_05e3960de2e9439999f39521c120e3df.cpython-xxx.pyc'), 'rb')
+s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '__custom_pycache__', 'NebulaPro_dfbf76f313024309b27be074b1be52b7.cpython-xxx.pyc'), 'rb')
 s.seek(16)
 exec(marshal.load(s))
